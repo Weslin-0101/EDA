d1d41740dcf9fef1460131a7bc1e7594a353e639
Weslin-0101 <wesleeylirac12@gmail.com>
2022-04-24 13:15:46 -0300

    Adição do exercício preOrdem

---
+++ lista4/preOrdem.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no
+{
+    int dado;
+    struct no *esq, *dir;
+} no;
+
+typedef struct celula
+{
+    no *dado;
+    struct celula *prox;
+} celula;
+
+celula *cria_pilha()
+{
+    celula *topo = malloc(sizeof(celula));
+    topo->prox = NULL;
+    return topo;
+}
+
+int pilha_vazia(celula *pilha)
+{
+    if (pilha->prox == NULL)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+void empilha(celula *p, no *x)
+{
+    celula *ponteiro = malloc(sizeof(celula));
+    ponteiro->dado = x;
+    ponteiro->prox = p->prox;
+    p->prox = ponteiro;
+}
+
+no *desempilha(celula *p)
+{
+    no *numero;
+    celula *lixo = p->prox;
+    numero = lixo->dado;
+    p->prox = lixo->prox;
+    free(lixo);
+    return numero;
+}
+
+void pre_ordem(no *raiz)
+{
+    if (raiz == NULL)
+    {
+        return;
+    }
+
+    celula *pilha = cria_pilha();
+    empilha(pilha, raiz);
+
+    while (pilha_vazia(pilha) != 0)
+    {
+        raiz = desempilha(pilha);
+        printf("%d ", raiz->dado);
+        if (raiz->dir != NULL)
+        {
+            empilha(pilha, raiz->dir);
+        }
+        if (raiz->esq != NULL)
+        {
+            empilha(pilha, raiz->esq);
+        }
+    }
+}
+
+/*
+no *cria_arvore(int x, no *esq, no *dir)
+{
+    no *raiz = malloc(sizeof(no));
+    raiz->dado = x;
+    raiz->esq = esq;
+    raiz->dir = dir;
+    return raiz;
+}
+
+no *procurar(no *raiz, int x)
+{
+    if (raiz == NULL || raiz->dado == x)
+        return raiz;
+    no *esq = procurar(raiz->esq, x);
+    if (esq != NULL)
+        return esq;
+    return procurar(raiz->dir, x);
+}
+
+void insere(no *raiz, int x, int y, char L)
+{
+    // Nós inserimos o x ao lado do L de y
+    no *procurado = procurar(raiz, y);
+    if (L == 'E')
+    {
+        no *novo = cria_arvore(x, procurado->esq, NULL);
+        procurado->esq = novo;
+    }
+    else
+    {
+        no *novo = cria_arvore(x, NULL, procurado->dir);
+        procurado->dir = novo;
+    }
+}
+
+int main()
+{
+    int elem, novo;
+    char lado;
+
+    scanf("%d", &novo);
+    no *raiz = cria_arvore(novo, NULL, NULL);
+
+    while (scanf("%d %c %d", &novo, &lado, &elem) != EOF)
+    {
+        insere(raiz, novo, elem, lado);
+    }
+
+    printf("Pre-ordem: ");
+    pre_ordem(raiz);
+    printf("\n");
+}
+*/
